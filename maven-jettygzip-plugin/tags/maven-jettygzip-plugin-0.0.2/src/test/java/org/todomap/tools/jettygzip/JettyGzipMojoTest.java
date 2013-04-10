74d6212b94884e97bcd8dd1cec237e4fb2185a1f
laszlo.hornyak@gmail.com <laszlo.hornyak@gmail.com@e834a378-9303-11de-97c9-d5744c8abab9>
2013-04-10 10:50:51 +0000

    [maven-release-plugin]  copy for tag maven-jettygzip-plugin-0.0.2

---
+++ maven-jettygzip-plugin/tags/maven-jettygzip-plugin-0.0.2/src/test/java/org/todomap/tools/jettygzip/JettyGzipMojoTest.java
@@ -0,0 +1,30 @@
+package org.todomap.tools.jettygzip;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileDeleteStrategy;
+import org.apache.commons.io.FileUtils;
+import org.junit.Before;
+import org.junit.Test;
+
+public class JettyGzipMojoTest {
+
+	File basedir;
+	
+	@Before
+	public void createWebappDirectoryStructure() throws IOException {
+		basedir = new File("target/testdir");
+		if(basedir.exists()) {
+			FileDeleteStrategy.FORCE.delete(basedir);
+		}
+		basedir.mkdirs();
+		FileUtils.copyDirectory(new File("src/test/resources"), basedir);
+	}
+
+	@Test
+	public void testSeekAndGzip() throws IOException {
+		new JettyGzipMojo().seekAndGzip(basedir);
+	}
+	
+}
