dd3d9e6fa914c31c9bcabcf049882a64530389f1
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2025-12-29 16:44:44 +0100

    minor cleanup on the kotlin code

--- src/main/kotlin/com/github/k0zka/contentcompress/FilesToGzipFilter.kt
+++ src/main/kotlin/com/github/k0zka/contentcompress/FilesToGzipFilter.kt
@@ -2,19 +2,12 @@ package com.github.k0zka.contentcompress
 
 import java.io.File
 import java.io.FilenameFilter
-import java.util.ArrayList
 
 internal class FilesToGzipFilter(
 		private val minSize: Long,
-		extensions: List<String>
+		private val extensions: List<String>
 ) : FilenameFilter {
-	private val extensions: List<String>
-
-	init {
-		this.extensions = ArrayList(extensions)
-	}
-
-	override fun accept(dir: File, name: String): Boolean {
+    override fun accept(dir: File, name: String): Boolean {
 		val file = File(dir, name)
 		return (matchesAny(name) && file.isFile
 				&& file.length() >= minSize)
